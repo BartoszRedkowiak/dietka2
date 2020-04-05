de05ce4a8f2113937e4a9df966c9415fde7a2e7a
BartoszRedkowiak <b.redkowiak@gmail.com>
2020-04-05 13:10:13 +0200

    Initial commit

---
+++ src/main/java/com/example/dietka/DietkaApplication.java
@@ -0,0 +1,13 @@
+package com.example.dietka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DietkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DietkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dietka/DietkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dietka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DietkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

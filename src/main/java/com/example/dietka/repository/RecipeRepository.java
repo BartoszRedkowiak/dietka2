99d32bd6c067d51af8cf58171fe139321afac53e
BartoszRedkowiak <b.redkowiak@gmail.com>
2020-04-11 12:45:35 +0200

    Repository classes initial commit

---
+++ src/main/java/com/example/dietka/repository/RecipeRepository.java
@@ -0,0 +1,9 @@
+package com.example.dietka.repository;
+
+import com.example.dietka.model.Recipe;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RecipeRepository extends JpaRepository<Recipe, Long> {
+}
